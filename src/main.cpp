5c603da4b1ac59c63011cef94f3cd5a73174f3c0
susui <13848950-sora_grayscale@users.noreply.gitlab.com>
2023-11-06 10:46:54 +0900

    add argcheck function, isnum

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,34 @@
 #include <cmath>
 
 // ./ircserv <port> <password>
-
-int main(int argc, char **argv)
+bool isnum(const std::string& input)
 {
-	int			port = 0;
-	std::string	password;
+	for (unsigned long i = 0; i < input.length(); i++)
+		if (!isdigit(input[i]))
+			return false;
+	return true;
+}
+
+int	check_port(std::string port);
+int	check_pass(std::string password);
+
+int	argcheck(std::string port, std::string password)
+{
+	if (check_port(port))
+		return 1;
+	if (check_pass(password))
+		return 1;
+	return 0;
+}
+
+int	main(int argc, char **argv)
+{
 
 	if (argc != 3)
 	{
 		std::cerr << "error usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
 	}
+	argcheck(argv[1], argv[2]);
 }
+
