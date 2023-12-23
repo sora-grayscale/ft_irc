0bad8813cf6b400d038b60bff7062b8adf622b13
susui <13848950-sora_grayscale@users.noreply.gitlab.com>
2023-12-23 19:29:09 +0900

    fix some

--- test/socket_programing/socket_c_lang/main.cpp
+++ test/socket_programing/socket_c_lang/main.cpp
@@ -20,7 +20,7 @@ int main() {
 
   // setting server config
   a_addr.sin_family = AF_INET;
-  a_addr.sin_port = htons(uint16_t(atoi("3368")));
+  a_addr.sin_port = htons(uint16_t(atoi("8080")));
   inet_aton("127.0.0.1", &a_addr.sin_addr);
 
   // setting config to socket
@@ -28,5 +28,11 @@ int main() {
 
   // wait connection
   listen(fd, 5);
+  
+  while (1)
+  {
+    std::cout << "wait connection" << std::endl;
+    int client_fd accept(fd, NULL, NULL);
+  }
 }
 
