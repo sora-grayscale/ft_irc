3356bc4b76d7a1cbd2b4cc2014e37374160c8d6d
susui <13848950-sora_grayscale@users.noreply.gitlab.com>
2023-12-23 18:59:19 +0900

    add comment out for explaing

--- test/socket_programing/socket_c_lang/main.cpp
+++ test/socket_programing/socket_c_lang/main.cpp
@@ -12,17 +12,21 @@ int main() {
   int fd;
   struct sockaddr_in a_addr;
 
+  // create socket
   fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd == -1)
     return 1;
   memset(&a_addr, 0, sizeof(struct sockaddr_in));
 
+  // setting server config
   a_addr.sin_family = AF_INET;
   a_addr.sin_port = htons(uint16_t(atoi("3368")));
   inet_aton("127.0.0.1", &a_addr.sin_addr);
 
+  // setting config to socket
   bind(fd, (struct sockaddr *)&a_addr, sizeof(a_addr));
 
+  // wait connection
   listen(fd, 5);
 }
 
