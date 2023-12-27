7873057299fc293365a5a7fbd5313a44924d0f19
susui <13848950-sora_grayscale@users.noreply.gitlab.com>
2023-12-27 20:19:48 +0900

    マルチユーザー行けたけどuserいなくなったあと消えない

--- test/main.cpp
+++ test/main.cpp
@@ -9,25 +9,28 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <poll.h>
+#include <vector>
 
 #define BUF_SIZE 1024
 
 // エラー処理ほぼないsocket programing
 
 // 本来なら終了を意味する何kが来るまでここで無限ループする
-void execute(int client_fd) {
+int execute(int client_fd) {
   int recv_size, send_size;
   char recv_buf[BUF_SIZE], send_buf;
 
-  while (1) {
+//  while (1) {
     recv_size = recv(client_fd, recv_buf, BUF_SIZE, 0);
     if (recv_size < 0) {
       if (errno == EWOULDBLOCK || errno == EAGAIN) {
         std::cerr << "No data received" << std::endl;
-        break;
+        return 1;
+//        break;
       } else {
         perror("recv");
-        break;
+        return 1;
+//        break;
       }
     }
     recv_buf[recv_size] = '\0';
@@ -38,17 +41,20 @@ void execute(int client_fd) {
       send_size = send(client_fd, &send_buf, 1, 0);
       if (send_size == -1) {
         std::cerr << "send error\n" << std::endl;
-        break;
+        return 1;
+//        break;
       }
     } else {
       send_buf = 1;
       send_size = send(client_fd, &send_buf, 1, 0);
       if (send_size == -1) {
         std::cerr << "send error\n" << std::endl;
-        break;
+        return 1;
+//        break;
       }
     }
-  }
+        return 0;
+//  }
 }
 
 int main() {
@@ -95,9 +101,10 @@ int main() {
   }
 
   // vectorの0にserverのfdを足す
-  fds.push_back(fd, POLLIN);
+  fds.push_back({fd, POLLIN});
 
-  while (1) {
+  while (1)
+  {
     // std::cout << "wait connection" << std::endl;
 
     // poll setting
@@ -106,24 +113,62 @@ int main() {
       perror("poll");
       break ;
     }
-    if (fds[0].revents != POLLIN)
 
-    int client_fd = accept(fd, NULL, NULL);
-
-    if (client_fd < 0) {
-      if (errno == EWOULDBLOCK || errno == EAGAIN) {
-        // std::cout << "No incomming connection" << std::endl;
-        continue;
-      } else {
-        perror("accept");
-        break;
+    if (fds[0].revents == POLLIN)
+    {
+      int client_fd = accept(fd, NULL, NULL);
+      if (client_fd < 0)
+      {
+        if (errno == EWOULDBLOCK || errno == EAGAIN)
+        {
+          // std::cout << "No incomming connection" << std::endl;
+          continue;
+        }
+        else
+        {
+          perror("accept");
+          break;
+        }
       }
-    } else {
-      std::cout << "conected" << std::endl;
-      execute(client_fd);
-      close(client_fd);
-      std::cout << "closed" << std::endl;
+      else
+      {
+        std::cout << "conected" << std::endl;
+        fds.push_back({client_fd, POLLIN});
+      }
     }
+    else
+    {
+      for (std::vector<pollfd>::iterator it = fds.begin(); it != fds.end(); ++it)
+      {
+        if ((*it).revents == POLLIN)
+        {
+          if (execute((*it).fd))
+          {
+            close((*it).fd);
+            std::cout << "closed" << std::endl;
+          }
+        }
+      }
+    }
+
+//    int client_fd = accept(fd, NULL, NULL);
+//
+//    if (client_fd < 0) {
+//      if (errno == EWOULDBLOCK || errno == EAGAIN) {
+//        // std::cout << "No incomming connection" << std::endl;
+//        continue;
+//      } else {
+//        perror("accept");
+//        break;
+//      }
+//    }
+//    else
+//    {
+//      std::cout << "conected" << std::endl;
+//      execute(client_fd);
+//      close(client_fd);
+//      std::cout << "closed" << std::endl;
+//    }
   }
   close(fd);
   return 0;
