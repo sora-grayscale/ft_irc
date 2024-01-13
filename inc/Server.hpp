13b200f2c84221b1701f805e2345a8b202fc088e
susui <13848950-sora_grayscale@users.noreply.gitlab.com>
2024-01-13 17:27:53 +0900

    add little bit

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -3,6 +3,9 @@
 
 #include "ircserv.hpp"
 
+// status
+#define NOT_REGISTERED 0
+
 class Command {
 private:
 public:
@@ -10,10 +13,11 @@ public:
 
 class User {
 private:
+  int fd;
   std::string nick;    // 9文字まで
   std::string service; // nick + server_name
-  int fd;
   std::string command;
+  int status; // statusをdefineしておくといいかも
 public:
   void set_fd(int fd) { this->fd = fd; }
   int get_fd() { return this->fd; }
