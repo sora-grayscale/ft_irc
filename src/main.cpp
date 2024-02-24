f2b18ec3f625ba4d81baac78ac73fa8698988fcd
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-02-24 18:29:36 +0900

    [feat]: #36 stat to wite main func
    [body]
    
    [notes]
    
    close #36
    Co-authored-by: enogawa <111509163+enogawa@users.noreply.github.com>
    Co-authored-by: yuki-shimoda <yuki-shimoda@protonmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include "Server.hpp"
+
+short validateAndConvertPort(const char *portStr) {
+  int port = 0;
+  size_t length = std::strlen(portStr);
+
+  if (length == 0 || length > 5)
+    throw std::runtime_error("port length is invalid");
+  for (size_t i = 0; i < length; ++i) {
+    if (!std::isdigit(portStr[i]))
+      throw std::runtime_error("Non-digit character found");
+    port = port * 10 + (portStr[i] - '0');
+  }
+  if (port < 1 || port > 65535)
+    throw std::runtime_error("port number is in the invalid range");
+  return static_cast<short>port;
+}
+
+std::string validateServerPassword(const char *password) {
+  const size_t MAX_LENGTH = 32;
+
+  if (password == NULL || *password == '\0') {
+    throw std::runtime_error("Password cannot be empty.");
+  }
+
+  size_t length = 0;
+  for (const char *p = password; *p != '\0'; ++p, ++length) {
+    if (!isalnum(*p) && *p != '_') {
+      throw std::runtime_error("Password contains invalid characters.");
+    }
+    if (length > MAX_LENGTH) {
+      throw std::runtime_error("Password is too long.");
+    }
+  }
+  return std::string(password, length);
+}
+
+Server::Server(int argc, char *argv[])
+{
+  if (argc != 3)
+    throw std::runtime_error("usage ./ircserv port password");
+}
+
+int main(int argc, char *argv[])
+{
+  try
+    Server server(argc, argv);
+  catch (const std::exception &e)
+    std::cout << "Error: " << e.what() << std::endl;
+}
