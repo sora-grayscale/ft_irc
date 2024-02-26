75beb0616f00be80cd4abcecf449dd29ca37596e
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-02-26 17:44:54 +0900

    [feat]: #96 create state&&user mode operator
    [body]
    
    [notes]
    
    close #86

--- inc/User.hpp
+++ inc/User.hpp
@@ -36,16 +36,18 @@ public:
 
   void setMode(unsigned int mode, bool enable);
   bool hasMode(ModeFlags flag) const;
+  void setState(RegisterState state);
+  RegisterState getState() const;
 
 private:
   std::string
-      nickname; // 1.2.1 ユーザー
+      _nickname; // 1.2.1 ユーザー
                 // 各ユーザーは、最大長さ9文字の一意のニックネームで他のユーザーと区別されます。ニックネームで使用される可能性のあるものとできないものについては、プロトコル文法規則（セクション2.3.1）を参照してください。
-  std::string username;
-  std::string realname; // 63 自分で決めた
-  const int userFd;
-  RegisterState state;
-  unsigned int modeFlags;
+  std::string _username;
+  std::string _realname; // 63 自分で決めた
+  const int _userFd;
+  RegisterState _state;
+  unsigned int _modeFlags;
 };
 
 #endif
