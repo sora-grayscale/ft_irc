5d4e144e701a47d834a64f09479cec2f449fc8d8
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-02-26 15:45:52 +0900

    fix: #81 ソケット初期化処理の修正
    ソケットの初期化処理で、アドレス構造体の初期化を`this`キーワードを用いて明示的に行い、ポート番号の設定で`htons`を使用して正しくネットワークバイトオーダーに変換するように修正しました。さらに、`bind`関数の呼び出しで`reinterpret_cast`を用いて型キャストを行うことで、コンパイルエラーを解消しました。
    
    これらの修正により、サーバーのソケット初期化時の正確性とポータビリティが向上しました。
    
    close #81

--- src/Server.cpp
+++ src/Server.cpp
@@ -74,12 +74,12 @@ void Server::initSocket() {
     throw std::runtime_error(std::strerror(errno));
   }
 
-  memset(&_addr, 0, sizeof(struct sockaddr_in));
+  memset(&this->_addr, 0, sizeof(struct sockaddr_in));
   _addr.sin_family = AF_INET;
   _addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  _addr.sin_port = htonl(this->port);
+  _addr.sin_port = htons(this->port);
 
-  if (bind(this->sfd, (struct sockaddr *)&this->_addr,
+  if (bind(this->sfd, reinterpret_cast<struct sockaddr *>(&this->_addr),
            sizeof(struct sockaddr_in)) == -1) {
     throw std::runtime_error(std::strerror(errno));
   }
