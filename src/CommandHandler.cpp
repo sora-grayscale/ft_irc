933e32ce0d74d154bfb0b3b272e58831a093ad09
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-02-28 17:16:56 +0900

    [fix]: #90 create pass command
    [body]
    fix param validation
    
    [notes]
    
    close #90

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -7,7 +7,7 @@ const std::string CommandHandler::PASS(const std::string &commandName,
                                        User &user) {
   if (user.getState() != User::NONE)
     return Replies::ERR_ALREADYREGISTRED();
-  if (params.size() != 1)
+  if (params.size() < 1)
     return Replies::ERR_NEEDMOREPARAMS(commandName);
   if (params.at(0) != this->_server.getPassword())
     return "";
