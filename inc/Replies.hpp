51455f9e1ec42a041a103480727024e1f302b8b7
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2024-03-07 14:18:56 +0900

    [feat]: #47 Repliesクラスへの追加と更新
    RepliesクラスにIRCプロトコルのレスポンスメッセージを追加し、更新しました。特に、`RPL_TOPIC`、`RPL_TOPICWHOTIME`、`RPL_ENDOFNAMES`、`ERR_NOSUCHCHANNEL`、`ERR_TOOMANYCHANNELS`などのメソッドを新たに実装しました。これにより、サーバーはクライアントへのレスポンスとして、チャンネルのトピックや名前リストの終了、存在しないチャンネルや参加済みのチャンネル数の超過などに関する情報を提供できるようになります。
    
    [body]
    - RPL_TOPIC: チャンネルのトピックをクライアントに通知。
    - RPL_TOPICWHOTIME: トピック設定者と設定時刻をクライアントに通知。
    - RPL_ENDOFNAMES: NAMESコマンドのレスポンス終了をクライアントに通知。
    - ERR_NOSUCHCHANNEL、ERR_TOOMANYCHANNELS: チャンネル参加に関するエラーをクライアントに通知。
    - その他、チャンネルが満員、招待のみ、BANされたユーザー、不正なチャンネルキー、不正なチャンネルマスクなどに対応するエラーメッセージを実装。
    
    [notes]
    これらの実装は、IRCサーバーとクライアント間のコミュニケーションをより豊かで正確にするために重要です。特に、チャンネルの管理やユーザーの状態に関する正確なフィードバックは、ユーザー体験の向上に直結します。
    
    close #47
    
    Co-authored-by: enogawa <111509163+enogawa@users.noreply.github.com>

--- inc/Replies.hpp
+++ inc/Replies.hpp
@@ -3,14 +3,24 @@
 
 #include <string>
 
+// 1. RPL_NAMREPLY (353)
 class Replies {
 public:
+  // 332
+  static const std::string RPL_TOPIC(const std::string &channelName,
+                                     const std::string &topic);
+  // 333
+  static const std::string RPL_TOPICWHOTIME(const std::string &channelName,
+                                     const std::string &nick,
+                                     const long &setat);
+  // 366
+  static const std::string RPL_ENDOFNAMES(const std::string &channelName);
+  // 403
+  static const std::string ERR_NOSUCHCHANNEL(const std::string &channelName);
+  // 405
+  static const std::string ERR_TOOMANYCHANNELS(const std::string &channelName);
   // 421
   static const std::string ERR_UNKNOWNCOMMAND(const std::string &command);
-
-  // 451
-  static const std::string ERR_NOTREGISTERED();
-
   // 431
   static const std::string ERR_NONICKNAMEGIVEN();
   // 432
@@ -23,15 +33,26 @@ public:
                                              const std::string &host);
   // 437
   static const std::string ERR_UNAVAILRESOURCE(const std::string &str);
-  // 484
-  static const std::string ERR_RESTRICTED();
-
+  // 451
+  static const std::string ERR_NOTREGISTERED();
   // 461
   static const std::string ERR_NEEDMOREPARAMS(const std::string &command);
   // 462
   static const std::string ERR_ALREADYREGISTRED();
   // 464
   static const std::string ERR_PASSWDMISMATCH();
+  // 471
+  static const std::string ERR_CHANNELISFULL(const std::string &channelName);
+  // 473
+  static const std::string ERR_INVITEONLYCHAN(const std::string &channelName);
+  // 474
+  static const std::string ERR_BANNEDFROMCHAN(const std::string &channelName);
+  // 475
+  static const std::string ERR_BADCHANNELKEY(const std::string &channelName);
+  // 476
+  static const std::string ERR_BADCHANMASK(const std::string &channelName);
+  // 484
+  static const std::string ERR_RESTRICTED();
 };
 
 #endif // REPLIES
--- src/Replies.cpp
+++ src/Replies.cpp
@@ -1,29 +1,72 @@
 #include "Replies.hpp"
 
+// 332
+const std::string Replies::RPL_TOPIC(const std::string &channelName, const std::string &topic) {
+  std::string message;
+  message += "332 ";
+  message += channelName;
+  message += " :";
+  message += topic;
+  message += "\r\n";
+  return (message);
+}
+
+// 333
+const std::string Replies::RPL_TOPICWHOTIME(const std::string &channelName, const std::string &nick, const long &setat) {
+  std::string message;
+  message += "333 ";
+  message += channelName;
+  message += " ";
+  message += nick;
+  message += " ";
+  message += setat;
+  message += "\r\n";
+  return (message);
+}
+
+
+// 366
+const std::string Replies::RPL_ENDOFNAMES(const std::string &channelName) {
+  std::string message;
+  message += "366 ";
+  message += channelName;
+  message += " :End of NAMES list";
+  message += "\r\n";
+  return (message);
+}
+
+// 403
+const std::string Replies::ERR_NOSUCHCHANNEL(const std::string &channelName) {
+  std::string message;
+  message += "403 ";
+  message += channelName;
+  message += " :No such channel";
+  message += "\r\n";
+  return (message);
+}
+
+// 405
+const std::string Replies::ERR_TOOMANYCHANNELS(const std::string &channelName) {
+  std::string message;
+  message += "405 ";
+  message += channelName;
+  message += " :You have joined too many channels";
+  message += "\r\n";
+  return (message);
+}
+
 // 421
 const std::string Replies::ERR_UNKNOWNCOMMAND(const std::string &command) {
   std::string message;
-
   message += "421 ";
   message += command;
   message += " :Unknown command";
   return (message);
 }
 
-
-// 451
-const std::string Replies::ERR_NOTREGISTERED() {
-  std::string message;
-
-  message += "451 ";
-  message += ":You have not registered";
-  return (message);
-}
-
 // 431
 const std::string Replies::ERR_NONICKNAMEGIVEN() {
   std::string message;
-
   message += "431 ";
   message += ":No nickname given";
   return (message);
@@ -32,7 +75,6 @@ const std::string Replies::ERR_NONICKNAMEGIVEN() {
 // 432
 const std::string Replies::ERR_ERRONEUSNICKNAME(const std::string &nick) {
   std::string message;
-
   message += "432 ";
   message += nick;
   message += " :Erroneous nickname";
@@ -42,7 +84,6 @@ const std::string Replies::ERR_ERRONEUSNICKNAME(const std::string &nick) {
 // 433
 const std::string Replies::ERR_NICKNAMEINUSE(const std::string &nick) {
   std::string message;
-
   message += "433 ";
   message += nick;
   message += " :Nickname is already in use";
@@ -51,10 +92,9 @@ const std::string Replies::ERR_NICKNAMEINUSE(const std::string &nick) {
 
 // 436
 const std::string Replies::ERR_NICKCOLLISION(const std::string &nick,
-                                           const std::string &user,
-                                           const std::string &host) {
+                                             const std::string &user,
+                                             const std::string &host) {
   std::string message;
-
   message += "436 ";
   message += nick;
   message += " :Nickname collision KILL from ";
@@ -69,36 +109,33 @@ const std::string Replies::ERR_NICKCOLLISION(const std::string &nick,
 // 437
 const std::string Replies::ERR_UNAVAILRESOURCE(const std::string &str) {
   std::string message;
-
   message += "437 ";
   message += str;
   message += " :Nick/channel is temporarily unavailable";
   return message;
 }
 
-// 484
-const std::string Replies::ERR_RESTRICTED() {
+// 451
+const std::string Replies::ERR_NOTREGISTERED() {
   std::string message;
-
-  message += "484 ";
-  message += ":Your connection is restricted!";
-  return message;
+  message += "451 ";
+  message += ":You have not registered";
+  return (message);
 }
 
 // 461
 const std::string Replies::ERR_NEEDMOREPARAMS(const std::string &command) {
   std::string message;
-
   message += "461 ";
   message += command;
   message += " :Not enough parameters";
+  message += "\r\n";
   return (message);
 }
 
 // 462
 const std::string Replies::ERR_ALREADYREGISTRED() {
   std::string message;
-
   message += "462 ";
   message += ":Unauthorized command (already registered)";
   return (message);
@@ -107,8 +144,65 @@ const std::string Replies::ERR_ALREADYREGISTRED() {
 // 464
 const std::string Replies::ERR_PASSWDMISMATCH() {
   std::string message;
-
   message += "464 ";
   message += ":Password incorrect";
   return (message);
+}
+
+// 471
+const std::string Replies::ERR_CHANNELISFULL(const std::string &channelName) {
+  std::string message;
+  message += "471 ";
+  message += channelName;
+  message += " :Cannot join channel (+l)";
+  message += "\r\n";
+  return (message);
+}
+
+// 473
+const std::string Replies::ERR_INVITEONLYCHAN(const std::string &channelName) {
+  std::string message;
+  message += "473 ";
+  message += channelName;
+  message += " :Cannot join channel (+i)";
+  message += "\r\n";
+  return (message);
+}
+
+// 474
+const std::string Replies::ERR_BANNEDFROMCHAN(const std::string &channelName) {
+  std::string message;
+  message += "474 ";
+  message += channelName;
+  message += " :Cannot join channel (+b)";
+  message += "\r\n";
+  return (message);
+}
+
+// 475
+const std::string Replies::ERR_BADCHANNELKEY(const std::string &channelName) {
+  std::string message;
+  message += "475 ";
+  message += channelName;
+  message += " :Cannot join channel (+k)";
+  message += "\r\n";
+  return (message);
+}
+
+// 476
+const std::string Replies::ERR_BADCHANMASK(const std::string &channelName) {
+  std::string message;
+  message += "476 ";
+  message += channelName;
+  message += " :Bad Channel Mask";
+  message += "\r\n";
+  return (message);
+}
+
+// 484
+const std::string Replies::ERR_RESTRICTED() {
+  std::string message;
+  message += "484 ";
+  message += ":Your connection is restricted!";
+  return message;
 }
