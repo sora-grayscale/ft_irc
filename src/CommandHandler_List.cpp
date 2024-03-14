66e105d9352b38150ca6f57982898519f3dd87fc
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2024-03-14 19:21:39 +0900

    [style]: #53 clang-format
    [body]
    
    [notes]
    
    close #53

--- src/CommandHandler_List.cpp
+++ src/CommandHandler_List.cpp
@@ -17,10 +17,10 @@ void CommandHandler::LIST(User &user) {
     for (std::map<std::string, Channel>::const_iterator it =
              this->_server.getChannelsBegin();
          it != this->_server.getChannelsEnd(); it++) {
-        this->_server.sendReply(
-            user.getFd(),
-            Replies::RPL_LIST(it->first, it->second.getVisibleUsrNum(user),
-                              it->second.getTopic()));
+      this->_server.sendReply(
+          user.getFd(),
+          Replies::RPL_LIST(it->first, it->second.getVisibleUsrNum(user),
+                            it->second.getTopic()));
       this->_server.sendReply(user.getFd(), Replies::RPL_LISTEND());
     }
     return;
@@ -33,10 +33,10 @@ void CommandHandler::LIST(User &user) {
       continue;
     }
     Channel &channel = this->_server.getChannel(channelNames.at(i));
-      this->_server.sendReply(user.getFd(),
-                              Replies::RPL_LIST(channel.getChannelName(),
-                                                channel.getVisibleUsrNum(user),
-                                                channel.getTopic()));
+    this->_server.sendReply(user.getFd(),
+                            Replies::RPL_LIST(channel.getChannelName(),
+                                              channel.getVisibleUsrNum(user),
+                                              channel.getTopic()));
     this->_server.sendReply(user.getFd(), Replies::RPL_LISTEND());
   }
   return;
