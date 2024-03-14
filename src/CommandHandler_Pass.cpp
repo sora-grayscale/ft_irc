12cbb692291ccf01f6a6240c79330f7bc01901f2
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-03-14 17:02:40 +0900

    [style]: #154 function division
    [body]
    
    [notes]
    
    close #154

---
+++ src/CommandHandler_Pass.cpp
@@ -0,0 +1,22 @@
+#include "CommandHandler.hpp"
+
+void CommandHandler::PASS(User &user) {
+  if (this->_params.size() == 0) {
+    this->_server.sendReply(user.getFd(),
+                            Replies::ERR_NEEDMOREPARAMS(this->_command));
+    return;
+  }
+  if (user.getState() != User::NONE && user.getState() != User::PASS) {
+    this->_server.sendReply(user.getFd(),
+                            Replies::Replies::ERR_ALREADYREGISTRED());
+    return;
+  }
+  if (this->_params.at(0) != this->_server.getPassword()) {
+    user.setState(User::PASS, false);
+    this->_server.sendReply(user.getFd(), Replies::ERR_PASSWDMISMATCH());
+    return;
+  }
+  user.setState(User::PASS, true);
+  return;
+}
+
