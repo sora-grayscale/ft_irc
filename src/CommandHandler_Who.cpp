501534eb481f1a7eeab706343fdb3ec2dff23d46
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-03-15 09:59:44 +0900

    [feat]: #73 create command who query
    [body]
    
    [notes]
    
    close #73

--- src/CommandHandler_Who.cpp
+++ src/CommandHandler_Who.cpp
@@ -1,3 +1,8 @@
 #include "CommandHandler.hpp"
 
-void CommandHandler::WHO(User &user) {}
+void CommandHandler::WHO(User &user) {
+  if (this->_params.size() == 0) {
+  } else if (this->_params.size() == 1) {
+  } else if (this->_params.size() == 2) {
+  }
+}
