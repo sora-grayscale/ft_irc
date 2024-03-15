12b7c32d84cc88d66b99e74859324290986e2938
susui <96704644+sora-grayscale@users.noreply.github.com>
2024-03-15 10:19:57 +0900

    [feat]: #73 create command who query
    [body]
    
    [notes]
    
    close #73

--- src/CommandHandler_Who.cpp
+++ src/CommandHandler_Who.cpp
@@ -1,8 +1,35 @@
 #include "CommandHandler.hpp"
 
+void displayWhoQuery(const int fd, const std::string &str, bool flag)
+{
+    if (str == '0')
+      // displayAllUser();
+    else if (str 実在するチャンネル)
+      // displayChannelUser();
+    else if (str == "127.0.0.1")
+      // displayAllUser();
+    else if (str == SERVER_NAME)
+      // displayAllUser();
+    else if (str == リアルネーム)
+      // displayUser();
+    else if (str == ニックネーム)
+      // displayUser();
+    else
+      this->_server.sendReply(fd, Replies::ERR_NOSUCHSERVER(str));
+
+}
+
 void CommandHandler::WHO(User &user) {
   if (this->_params.size() == 0) {
-  } else if (this->_params.size() == 1) {
-  } else if (this->_params.size() == 2) {
+    // displayAllUser();
   }
+  else if (this->_params.size() == 1) {
+    displayWhoQuery(user.getFd(), this->_params.at(0), false);
+    return ;
+  }
+  else if (this->_params.size() >= 2) {
+    if (this->_params.at(1) == 'o')
+      displayWhoQuery(user.getFd(), this->_params.at(0), true);
+  }
+  this->_server.sendReply(user.getFd(), Replies::ERR_NOSUCHSERVER(str));
 }
